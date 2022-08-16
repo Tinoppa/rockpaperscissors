dfe49f8fb0296ab0e2e59140b3ac7e75a49de5d4
Tinoppa <tino.kalliola@gmail.com>
2022-08-16 14:46:49 +0300

    Add rest of the logic and functionality

--- script.js
+++ script.js
@@ -4,22 +4,32 @@ let options = ["Rock", "Paper", "Scissors"];
 // Declare scores
 let playerScore = 0;
 let computerScore = 0;
+let playerScoreBoard = document.getElementById('player-score');
+let computerScoreBoard = document.getElementById('computer-score');
+const resetBtn = document.getElementById('reset');
+const roundResult = document.getElementById('score-para');
 // Create a function for computers pick, that is random
 function getComputerChoice(){
 let computerSelection = options[Math.floor(Math.random() * options.length )].toLowerCase();
 return computerSelection;
 }
-
+resetBtn.addEventListener('click', init);
 // Create variables for player and computer selections
 const buttons = document.getElementsByTagName("button");
-//let playerSelection = '';
+playerSelection = '';
 let computerSelection = getComputerChoice();
 let rock = document.getElementById('rock');
 let paper = document.getElementById('paper');
 let scissors = document.getElementById('scissors');
 // Create logic for the game = Rock beats scissors, Paper beats Rock, Scissors beat paper, ties are ties
 
-
+function init() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreBoard.textContent = '0';
+    computerScoreBoard.textContent = '0';
+    roundResult.textContent = ''
+}
 
 rock.addEventListener('click',function(){
     playerSelection = 'rock'
@@ -40,59 +50,66 @@ scissors.addEventListener('click',function(){
 
 // Single round of RSP in the console
 function singleGame (playerSelection, computerSelection) {
-   
+
     console.log(computerSelection)
-     if (playerSelection === "rock" && computerSelection === "paper") {
+    if (playerScore == 5 || computerScore == 5) {
+        init();
+        alert ('Game over')
+    }
+     else if (playerSelection === "rock" && computerSelection === "paper") {
        // console.log("Paper beats Rock");
         computerScore += 1;
+        computerScoreBoard.textContent = computerScore;
         console.log(computerScore)
-        return "Paper beats Rock";
+        roundResult.textContent = 'You Lose!'
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
       //  console.log("Scissors beat Rock");
-        computerScore += 1;
-        return "Scissors beat Rock";
+        playerScore += 1;
+        playerScoreBoard.textContent = playerScore;
+        roundResult.textContent = 'You Win!'
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
       //  console.log("Scissors beat Paper");
         computerScore += 1;
-        return "Scissors beat Paper";
+        computerScoreBoard.textContent = computerScore;
+        roundResult.textContent = 'You Lose!'
     } else if (playerSelection === "paper" && computerSelection === "rock") {
      //   console.log("Paper beat Rock");
         playerScore += 1;
-        return "Paper beats Rock";
+        playerScoreBoard.textContent = playerScore;
+        roundResult.textContent = 'You Win!'
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
       //  console.log("Scissors beat Paper");
         playerScore += 1;
-        return "Scissors beat Paper";
+        playerScoreBoard.textContent = playerScore;
+        roundResult.textContent = 'You Win!'
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
      //   console.log("Rock beats Scissors");
         computerScore += 1;
-        return "Rock beats Scissors";
+        computerScoreBoard.textContent = computerScore;
+        roundResult.textContent = 'You Lose!'
     } else if (playerSelection === computerSelection) {
      //   console.log("It's a tie!");
-        return "It's a tie";
+     roundResult.textContent = "It's a Tie!"
         // If player doesn't choose between the correct options, show this message
-    } else if (playerSelection != options) {
-       // console.log("Please choose between Rock, Paper or Scissors");
-        return "Please choose between Rock, Paper or Scissors"
-    } 
+    }
    
+
 } 
 
 // Create new function that loops 5 times and keeps score
 
 function game () {
 
-    playerSelection = prompt("Select your weapon").toLowerCase()
-    computerSelection = getComputerChoice();
+    //computerSelection = getComputerChoice();
     
     for (let i = 0; i < 5; i++) {
         singleGame(playerSelection, computerSelection);
         console.log(playerScore)
         console.log(computerScore)
-        computerSelection = getComputerChoice();
-        playerSelection = prompt("Select your weapon").toLowerCase()
+       /* computerSelection = getComputerChoice();
+        playerSelection = prompt("Select your weapon").toLowerCase()*/
     }
-
+    init();
     if (playerScore > computerScore) {
         console.log("You won, amazing stuff man!")
     } else if (playerScore < computerScore) {
@@ -104,6 +121,8 @@ function game () {
 }
 
 
+
+
 //game();
 
 
@@ -111,6 +130,6 @@ function game () {
 /* 
 1. I need to store the ID of button clicked by user as the users selection
 2. Once user clicks a button, run the function of the game
-3. 
-
+3. Add variables for score fields
+4. Reset the game when either player reaches 5 points
 */
